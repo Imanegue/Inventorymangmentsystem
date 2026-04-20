ebb1e4bd7039230ad426195f30fc0d9b80c38611
Imanegue <i.guelmamene@etu.univ-batna2.dz>
2026-04-20 20:26:27 +0100

    Update app.js :Adding order routes and category routes

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,9 +1,10 @@
 import express from "express";
 import cors from "cors";
 import { errorHandler } from "./middleware/errorHandler.js";
+import orderRoutes from "./routes/orders.js";
 import productRoutes from "./routes/productRoutes.js";
-import categoryRoutes from "./routes/categoryRoutes.js";
 import supplierRoutes from "./routes/supplierRoutes.js";
+import categoryRoutes from "./routes/categories.js";
 
 const app = express();
 
@@ -15,6 +16,8 @@ app.use(express.json());
 app.use("/api/products", productRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/suppliers", supplierRoutes);
+app.use("/api/order", orderRoutes);
+
 
 // ─── ERROR HANDLER ────────────────────────────
 app.use((req, res, next) => {
